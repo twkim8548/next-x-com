1277edf160242ccf240efd27bf90a2c25afb96ab
김태웅 <rlaxodnd95@gmail.com>
2023-12-14 00:32:33 +0900

    Css Module 설정, Home 화면 설정

--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main >
-      홈 페이지
+      로그인
     </main>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import styles from "@/app/page.module.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        루트 레이아웃
+      <div className={styles.container}>
         {children}
+      </div>
       </body>
     </html>
   )
